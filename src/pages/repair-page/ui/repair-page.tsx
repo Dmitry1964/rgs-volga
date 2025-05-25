6aebdf83ff1504c2225589c041f44440e9067a1d
Dmytry64 <nosdm64@gmail.com>
2025-05-25 08:46:59 +0300

    Добавляет repair-page

--- src/pages/repair-page/ui/repair-page.tsx
+++ src/pages/repair-page/ui/repair-page.tsx
@@ -1,7 +1,11 @@
-const RepairPage = () : React.ReactNode => {
-    return (
-        <div></div>
-    )
-}
+import { Intro } from "src/widgets/intro";
+
+const RepairPage = (): React.ReactNode => {
+  return (
+    <Intro>
+      <h2>Ремонт гидравлического оборудования</h2>
+    </Intro>
+  )
+};
 
 export default RepairPage;
--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -3,11 +3,15 @@ import cls from "./header.module.scss";
 import { Navbar } from "src/features/navbar";
 import useViewportWidth from "src/hooks/useViewportWidth";
 import { TABLET_VIEWPORT } from "src/shared/lib/constans";
+import { classNames } from "src/shared/lib/class-names";
+import { useLocation } from "react-router-dom";
+import { AppRoutes } from "src/app/routes/routes";
 
 const Header = (): React.ReactNode => {
-  const screenWidth = useViewportWidth()
+  const screenWidth = useViewportWidth();
+  const isMainPage = useLocation().pathname === AppRoutes.Main;
   return (
-    <header className={cls.header}>
+    <header className={classNames(cls.header, [], { [cls.header__main_page]: isMainPage })}>
       <HeaderContacts />
       {screenWidth >= TABLET_VIEWPORT && <Navbar />}
     </header>
