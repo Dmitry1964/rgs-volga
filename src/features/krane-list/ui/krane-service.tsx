779742cbc97bcb5e690ee52c2bb4f33bad16467d
Dmytry64 <nosdm64@gmail.com>
2025-07-05 21:35:12 +0300

    Добавляет анимацию секции krane-service

--- src/features/krane-list/ui/krane-service.tsx
+++ src/features/krane-list/ui/krane-service.tsx
@@ -1,9 +1,9 @@
 import { FC, useRef } from "react";
-import cls from './krane-list.module.scss';
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useGSAP } from "@gsap/react";
-
+import './krane-section.scss';
+import cls from './krane-list.module.scss';
 
 
 const KraneService: FC = () => {
@@ -15,8 +15,9 @@ const KraneService: FC = () => {
 
   useGSAP(() => {
 
-    tl.from('#krane', { yPercent: -100 });
-    tl.from('#kmu', { yPercent: 100 });
+    // tl.from('.scroll-sections__item--blue', { yPercent: 0 });
+    tl.from('#section-kmu', { yPercent: 100, opacity: 1 });
+    tl.from('#section-vipo', { yPercent: 100, opacity: 1 });
 
     ScrollTrigger.create({
       animation: tl,
@@ -24,79 +25,58 @@ const KraneService: FC = () => {
       start: 'top top',
       end: '+=4000',
       scrub: true,
-      markers: true,
       pin: true,
       anticipatePin: 1,
     });
-  }, { scope: container });
+  });
+
 
   return (
-    <section className={cls.krane_service} ref={container}>
-      <div className={cls.krane_service__container}>
-        <div id='krane' className={cls.krane_service__wrapper}>
-          <div className={cls.krane_service__banner}>
-            <picture>
-              <source srcSet="img/content/krane-banner.webp 1x, img/content/krane-banner@2x.webp 2x" type="image/webp" />
-              <img className={cls.krane_service__image} srcSet="img/content/krane-banner.jpg 1x, img/content/krane-banner@2x.jpg 2x" alt="Баннер автокран" />
-            </picture>
-          </div>
-          <div className={cls.krane_service__content}>
-            <div className='container'>
-              <h2 className={cls.krane_service__title}>Ремонт и сервис автокранов</h2>
-              {/* <p className={cls.krane_service__text}>
-              Мы предлагаем широкий спектр услуг по ремонту и сервису автокранов, включая текущий ремонт и техническое обслуживание. Наши специалисты имеют большой опыт в ремонте и обслуживании автокранов различных марок и моделей, что позволяет нам предлагать качественные и эффективные решения для ваших задач.
-            </p>
-            <ul className={cls.krane_service__list}>
-              <li className={cls.krane_service__item}>
-                <span>Ремонт гидравлического оборудования</span>
-              </li>
-              <li className={cls.krane_service__item}>
-                <span>Ремонт механизма поворота</span>
-              </li>
-              <li className={cls.krane_service__item}>
-                <span>Ремонт механизма выдвижения стрелы</span>
-              </li>
-              <li className={cls.krane_service__item}>
-                <span>Ремонт и диагностика электрооборудования</span>
-              </li>
-            </ul> */}
+    <section className={cls.krane_sections}>
+      <div className={cls.krane_sections__list} ref={container}>
+        <div className={cls.krane_sections__item}>
+          <div className={cls.krane_sections__content}>
+            <div className="container">
+              <h2 className={cls.krane_sections__title}>Ремонт и обслуживание автокранов</h2>
             </div>
           </div>
-        </div>
-        <div id='kmu' className={cls.krane_service__wrapper}>
-          <div className={cls.krane_service__banner}>
+          <div className={cls.krane_sections__banner}>
             <picture>
-              <source srcSet="img/content/kmu-banner.webp 1x, img/content/kmu-banner@2x.webp 2x" type="image/webp" />
-              <img className={cls.krane_service__image} srcSet="img/content/kmu-banner.jpg 1x, img/content/kmu-banner@2x.jpg 2x" alt="Баннер автокран" />
+              <source srcSet="img/content/krane-banner.webp, img/content/krane-banner@2x.webp 2x" type="image/webp" />
+              <img className={cls.krane_sections__img} src="img/content/krane-banner.jpg" srcSet="img/content/krane-banner@2x.jpg 2x" alt="Изображение автокрана" />
             </picture>
           </div>
-          <div className={cls.krane_service__content}>
-            <div className='container'>
-              <h2 className={cls.krane_service__title}>Ремонт и сервис краново-манипуляторных установок</h2>
-              {/* <p className={cls.krane_service__text}>
-              Мы предлагаем широкий спектр услуг по ремонту и сервису краново-манипуляторных установок, включая текущий ремонт и техническое обслуживание. Наши специалисты имеют большой опыт в ремонте и обслуживании краново-манипуляторных установок различных марок и моделей, что позволяет нам предлагать качественные и эффективные решения для ваших задач.
-            </p>
-            <ul className={cls.krane_service__list}>
-              <li className={cls.krane_service__item}>
-                <span>Ремонт гидравлического оборудования</span>
-              </li>
-              <li className={cls.krane_service__item}>
-                <span>Ремонт механизма поворота</span>
-              </li>
-              <li className={cls.krane_service__item}>
-                <span>Ремонт механизма выдвижения стрелы</span>
-              </li>
-              <li className={cls.krane_service__item}>
-                <span>Ремонт и диагностика электрооборудования</span>
-              </li>
-            </ul> */}
+        </div>
+        <div id="section-kmu" className={cls.krane_sections__item}>
+          <div className={cls.krane_sections__content}>
+            <div className="container">
+              <h2 className={cls.krane_sections__title}>Ремонт и обслуживание кранов-манипуляторов</h2>
             </div>
           </div>
+          <div className={cls.krane_sections__banner}>
+            <picture>
+              <source srcSet="img/content/kmu-banner.webp, img/content/kmu-banner@2x.webp 2x" type="image/webp" />
+              <img className={cls.krane_sections__img} src="img/content/kmu-banner.jpg" srcSet="img/content/kmu-banner@2x.jpg 2x" alt="Изображение автокрана" />
+            </picture>
+          </div>
         </div>
+        <div id="section-vipo" className={cls.krane_sections__item}>
+          <div className={cls.krane_sections__content}>
+            <div className="container">
+              <h2 className={cls.krane_sections__title}>Ремонт и обслуживание автовышек</h2>
+            </div>
+          </div>
+          <div className={cls.krane_sections__banner}>
+            <picture>
+              <source srcSet="img/content/vipo-banner.webp, img/content/vipo-banner@2x.webp 2x" type="image/webp" />
+              <img className={cls.krane_sections__img} src="img/content/vipo-banner.jpg" srcSet="img/content/vipo-banner@2x.jpg 2x" alt="Изображение автокрана" />
+            </picture>
+          </div>
+        </div>
+
+
       </div>
-
-
-    </section>
+    </section >
   )
 }
 
--- src/widgets/service-section/ui/service-section.tsx
+++ src/widgets/service-section/ui/service-section.tsx
@@ -1,51 +1,11 @@
-import { FC, useRef } from "react";
-import './service-section.scss';
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { useGSAP } from "@gsap/react";
-gsap.registerPlugin(ScrollTrigger);
+import { FC} from "react";
+import { KraneService } from "src/features/krane-list";
 
 const ServiceSection: FC = () => {
-  const container = useRef<HTMLDivElement>(null);
-  const tl = gsap.timeline();
-  gsap.registerPlugin(ScrollTrigger);
-
-
-  useGSAP(() => {
-
-    // tl.from('.scroll-sections__item--blue', { yPercent: 0 });
-    tl.from('.scroll-sections__item--orange', { yPercent: 100, opacity: 0 });
-    tl.from('.scroll-sections__item--green', { yPercent: 100, opacity: 0 });
-
-    ScrollTrigger.create({
-      animation: tl,
-      trigger: container.current,
-      start: 'top top',
-      end: '+=4000',
-      scrub: true,
-      markers: true,
-      pin: true,
-      anticipatePin: 1,
-    });
-  });
-
-
 
   return (
-    <section className='scroll-sections'>
-      <div className='scroll-sections__list' ref={container}>
-        <div className='scroll-sections__item scroll-sections__item--blue blue'>
-          <span>Blue</span>
-        </div>
-        <div className='scroll-sections__item scroll-sections__item--orange orange'>
-          <span>Orange</span>
-        </div>
-        <div className='scroll-sections__item scroll-sections__item--green green'>
-          <span>Green</span>
-        </div>
-      </div>
-    </section>
-  );
+    <KraneService />
+  )
 };
 
 export default ServiceSection;
